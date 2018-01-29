53a770580ecd83efa27ad465b8b09e6ff7c7369e
l-ribeiro <luizaugustovr@gmail.com>
2018-01-29 18:26:33 -0200

    Commit da aplicação de bate-papo
    Commit da aplicação de bate-papo.
    Estudando node.js

---
+++ bate-papo/index.js
@@ -0,0 +1,38 @@
+// Iniciamos uma nova instância do socket.io passando o objeto http(o servidor http)
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+ 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+ 
+// Quando tivermos uma chamada para o nosso socket iremos logar no terminal uma mensagem de novo usuário
+/*
+io.on('connection', function(socket){
+  console.log('Usuário conectado.');
+  socket.on('disconnect', function(){
+    console.log('O usuário saiu da aplicação.');
+  });
+});
+*/
+
+/*
+io.on('connection', function(socket){
+  socket.on('mensagem de chat', function(msg){
+    console.log('nova mensagem enviada: ' + msg);
+  });
+});
+*/
+
+
+io.on('connection', function(socket){
+  socket.on('mensagem de chat', function(msg){
+    io.emit('mensagem de chat', msg);
+  });
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
